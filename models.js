e67aacb7d6a2f28f198199c0153dd9aad6e5050d
Felipe S. Fernandes <feleast.49@gmail.com>
2017-10-17 00:23:25 -0300

    Parser update

--- models.js
+++ models.js
@@ -30,3 +30,36 @@ exports.Schedule = mongoose.model('Schedule', {
 	observacao: String,
 	created: Date
 });
+
+exports.Hotran = mongoose.model('Hotran', {
+	cod_empresa: String,
+	nome_empresa: String,
+	voo: Number,
+	aeronave: String,
+	dias: {
+		segunda_feira: {type: Boolean, default: false},
+		terca_feira: {type: Boolean, default: false},
+		quarta_feira: {type: Boolean, default: false},
+		quinta_feira: {type: Boolean, default: false},
+		sexta_feira: {type: Boolean, default: false},
+		sabado: {type: Boolean, default: false},
+		domingo: {type: Boolean, default: false},
+	},
+	assentos: Number,
+	cod_hotran: String,
+	tipo: String,
+	status: String,
+	data_solicitacao: Date,
+	data_aprovacao: Date,
+	data_vigencia: Date,
+	cod_hotran_anterior: String,
+	natureza: String,
+	etapa: Number,
+	cod_origem: String,
+	aeroporto_origem: String,
+	cod_destino: String,
+	aeroporto_destino: String,
+	horario_partida: String,
+	horario_chegada: String,
+	equipamento_alterado: String
+});
--- settings.js
+++ settings.js
@@ -1,3 +1,8 @@
 var fs = require('fs');
 exports.csvfile = "data/hotran.csv";
 exports.logfile = 'data/log.txt';
+exports.hotrans = {
+	semParecer: { url: 'http://www2.anac.gov.br/arquivos/xls/hotran/1.1.xls', path: 'data/1.1.xls' },
+	comParecer: { url: 'http://www2.anac.gov.br/arquivos/xls/hotran/1.2.xls', path: 'data/1.2.xls' },
+	aVigorar: { url: 'http://www2.anac.gov.br/arquivos/xls/hotran/1.4.xls', path: 'data/1.4.xls' }
+};
