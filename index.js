969e54232a6f49df781ca0f3037c3a5be276de0a
Felipe S. Fernandes <feleast.49@gmail.com>
2017-10-22 01:17:09 -0300

    Alterações de layout e inclusão de busca e mais detalhamentos.

--- index.js
+++ index.js
@@ -43,62 +43,60 @@ mongoose.connect(connectionString, {useMongoClient: true, promiseLibrary: global
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.locals.moment = require('moment');
+app.locals.moment = require('moment-timezone');
 app.use(express.static(__dirname + '/public'));
 app.listen( settings.serverPort, function() {
 	console.log("servidor iniciado na porta "+settings.serverPort);	
 });
 
 app.get('/', function(req, res) {
-	var today = moment().tz('America/Fortaleza');
-	var hotranUpdates = { created_at: undefined, message: 'Sem recentes atualizações de hotran.' };		
-		models.Log.find({
-			type: settings.logCategories.hotran,
-		})
-		.sort({'created_at': -1})
-		.limit(1)
-		.exec(function(err, log) {			
-			if(log.length) {
-				var date = moment(log[0].created_at, 'DD-MM-YYYY HH:mm:ss');
-				hotranUpdates.message = date.format('DD/MM/YYYY') + " as " + date.format("HH:mm:ss");
-			}			
-			res.render('pages/index', {				
-				hotranUpdates: hotranUpdates
-			});
-		});	
-	/*models.Hotran.find({
-		data_solicitacao: "2017-10-18"//today.format('YYYY-MM-DD')
-	}, (err, hotrans) => {
-		if(err) throw err;
-		models.Log.find({
-			type: settings.logCategories.hotran,
-		})
-		.sort({'created_at': -1})
-		.limit(1)
-		.exec(function(err, log) {
-			if( log != null ) {
-				var hotranLastUpdate = moment(log[0].created_at, 'DD-MM-YYYY HH:mm:ss');
-				hotranUpdate = hotranLastUpdate.format('DD/MM/YYYY') + " as " + hotranLastUpdate.format("HH:mm:ss");
-			}
-			res.render('pages/index', {
-				hotranUpdate: hotranUpdate,
-				hotrans: hotrans,
-				year: today.format('YYYY') 
-			});
-		});		
-	})*/
-});
-app.get('/hotran/all', function(req, res) {
-	models.Hotran.find({})
-	.sort({'nome_empresa': 1})
+	var today = moment().tz('America/Fortaleza').format('DD-MM-YYYY');
+	var lastUpdate = { created_at: undefined, message: 'Sem recentes atualizações de hotran.' };		
+	models.Log.find({ // Catching hotran log.
+		type: settings.logCategories.hotran,
+	})
+	.sort({'created_at': -1})		
 	.limit(1)
-	.exec((err, hotrans) => {
-		async.eachLimit(hotrans, 1, (hotran, callback) => {
-			hotran.data_solicitacao = 'x';//moment(hotran.data_solicitacao).tz('UTC').format('YYYY-MM-DD');
-			console.log(hotran.data_solicitacao);
-			process.nextTick(callback);
-		}, function() {
-			res.json(hotrans);
+	.exec(function(err, log) {		
+		if(log.length) {
+			var date = moment(log[0].created_at, 'DD-MM-YYYY HH:mm:ss');
+			lastUpdate.message = date.format('DD/MM/YYYY') + " as " + date.format("HH:mm:ss");
+		}
+		res.render('pages/index', {
+			lastUpdate: lastUpdate,
+			today: today
 		});		
+	});	
+});
+app.get('/hotran/fields', function(req, res) {
+	if(req.query.field === undefined) { res.end(); return; }	
+	var params = req.query;	
+	var query = { groupBy: {}, orderBy: {} };
+	query.groupBy[params.field] = "$".concat(params.field);
+
+	models.Hotran.aggregate([		
+		{$group: {"_id":query.groupBy}},
+		{$sort: {"_id":1}}
+		], (err, result) => {		
+			var filtered = result.map(obj => {
+				return obj['_id'][params.field].trim();
+			});		
+			res.json(filtered);
+		});
+
+});
+app.get('/hotran/api', function(req, res) {
+	const limit = 700;
+	var params = req.query;	
+	Object.keys(params).forEach(key => {		
+		if(!params[key].length) delete params[key];
+		else if(key === "data_solicitacao") params[key] = moment(params[key], 'DD-MM-YYYY').tz('America/Fortaleza').format('YYYY-MM-DD');
+	});	
+	models.Hotran.find(params)	
+	.sort({'nome_empresa': 1})
+	.limit(limit)
+	.exec((err, hotrans) => {
+		if(err) throw err;
+		res.json(hotrans);
 	});
 });
